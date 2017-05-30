4d3236f646ed7289039f1f8a0891b091e98669b9
DenDrummer <muzikant96@gmail.com>
2017-05-30 16:52:40 +0200

    V2

--- src/app.js
+++ src/app.js
@@ -4,8 +4,60 @@
 import './css/custom.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery'
+var show_navbar = false;
 
-$(".home-button").click(function () {
+$(document).ready(function () {
+    $(".home").show();
+    $(".about").hide();
+
+    //load team members
+    $.getJSON(`http://localhost:3000/team-members`,
+        (json) => {
+            console.log(json);
+            var stringified = JSON.stringify(json);
+            if (stringified == "") {
+                //list does not exist
+                $("#tmp").val("It seems there is no list of team-members.");
+            }
+            else if (stringified == "[]") {
+                //list is empty
+                $("#tmp").val("It seems the list of team-members is empty.");
+            }
+            else {
+                $.each(json, (index, element) => {
+                    $("#tmp").append($('<div>', {
+                        text: element.name
+                    }))
+                })
+            }
+        })
+});
+
+//$(window).resize(() => {
+//if ($(window).width() < 768) {
+//$(".hidden").hide();
+//} else {
+//$(".hidden").show();
+//}
+//});
+
+$(".navbar-toggle").click(() => {
+    if (show_navbar == true) {
+        show_navbar = false;
+        $("#navbar").addClass("hidden");
+    } else {
+        show_navbar = true;
+        //$(".hidden").show();
+        $("#navbar").removeClass("hidden");
+    }
+    //if ($(window).width() < 768) {
+    //$(".hidden").hide();
+    //} else {
+    //$(".hidden").show();
+    //}
+});
+
+$(".home-button").click(() => {
     $(".home-button").addClass("active");
     $(".about-button").removeClass("active");
 
@@ -13,7 +65,7 @@ $(".home-button").click(function () {
     $(".about").hide();
 });
 
-$(".about-button").click(function () {
+$(".about-button").click(() => {
     $(".home-button").removeClass("active");
     $(".about-button").addClass("active");
 
